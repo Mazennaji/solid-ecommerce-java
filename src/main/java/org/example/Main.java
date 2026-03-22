a3c7b2074aad580be1b97d7c2b488e8ead97aec4
Mazen Naji <mazen.naji.1@gmail.com>
2026-03-22 14:43:52 +0200

    Integrate all components in Main class and test order flow

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
+import org.example.controller.OrderProcessor;
+import org.example.model.Order;
+import org.example.model.Product;
+import org.example.service.notification.EmailNotification;
+import org.example.service.payment.PayPalPayment;
+import org.example.service.pricing.DiscountPricing;
+
+import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Product p1 = new Product("Laptop", 1000);
+        Product p2 = new Product("Mouse", 50);
+
+        Order order = new Order(Arrays.asList(p1, p2));
+
+        OrderProcessor processor = new OrderProcessor(
+                new DiscountPricing(),
+                new PayPalPayment(),
+                new EmailNotification()
+        );
+
+        processor.process(order);
     }
 }
